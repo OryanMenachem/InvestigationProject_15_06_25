ad6172975ad191a489e36c6b65e83fcc66b0e1fa
Oryan Menachem <om3160204@gmail.com>
2025-06-15 15:11:15 +0300

    Partial realization of Iranian agent generator

---
+++ InvestigationProject_15_06_25/IranianAgentGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationProject_15_06_25
+{
+    internal class IranianAgentGenerator
+    {
+        private static Random random = new Random();
+        public static IIranianAgent GetIranianAgent()
+        {
+            IIranianAgent iranianAgent = null;
+
+            switch (random.Next(0,1)) //
+            {
+                case 0:
+                    iranianAgent = new JuniorIranianAgent("h",new List<ISensors>());
+                    break;
+
+
+            }
+            return iranianAgent;
+        }
+
+    }
+}
