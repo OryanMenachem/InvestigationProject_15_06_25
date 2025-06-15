f0d442d3daea56a74349fc1e4aa86196c3cb94b2
Oryan Menachem <om3160204@gmail.com>
2025-06-15 14:04:46 +0300

    Creating a database of Iranian agent names

---
+++ InvestigationProject_15_06_25/NamesOfIranianAgents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationProject_15_06_25
+{
+    internal class NamesOfIranianAgents
+    {
+
+        public string[] firstName { get; } = {
+                    "Mohammad", "Fatemeh", "Zahra", "Ali", "Reza",
+                    "Sara", "Maryam", "Masoud", "Nima", "Omid",
+                    "Arash", "Parvīz", "Farhad", "Babak", "Kaveh",
+                    "Darius", "Ehsan", "Kamran", "Amin", "Hossein" };
+
+        public string[] lastName { get; } ={
+                    "Mohammadi", "Hosseini", "Ahmadi", "Ali", "Moradi",
+                    "Karimi", "Mousavi", "Jafari", "Rahimi", "Ebrahimi",
+                    "Abbasi", "Gholami", "Ghasemi", "Ghorbani", "Akbari",
+                    "Sadeghi", "Gholami", "Shirazi", "Tehrani", "Sharifi"
+                };
+
+
+
+    }
+}
