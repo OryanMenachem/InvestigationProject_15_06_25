a477cba09b61cb108eaa21685c0cc70f979cccb2
Oryan Menachem <om3160204@gmail.com>
2025-06-15 14:24:00 +0300

    Creating interfaces for Iranian agents and censors

---
+++ InvestigationProject_15_06_25/IIranianAgent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationProject_15_06_25
+{
+    internal interface IIranianAgent
+    {
+        string Name { get; }
+
+        List<ISensors> SensorsList { get; }
+    }
+}
