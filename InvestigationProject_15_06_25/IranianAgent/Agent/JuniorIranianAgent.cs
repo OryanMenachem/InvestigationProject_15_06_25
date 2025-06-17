4302801631838b80e54513c6ec680e7a4f2a1c8f
Oryan Menachem <om3160204@gmail.com>
2025-06-17 16:38:50 +0300

    Adding 'SquadLeader' class

--- InvestigationProject_15_06_25/IranianAgent/Agent/JuniorIranianAgent.cs
+++ InvestigationProject_15_06_25/IranianAgent/Agent/JuniorIranianAgent.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace InvestigationProject_15_06_25
 {
+
     internal class JuniorIranianAgent : IIranianAgent
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Rank { get; } = "Junior";
-        public List<string> SensitiveToSensors { get; set; } = new List<string>(); // רק שני סנסורים לחשיפה
+        public List<string> SensitiveToSensors { get; set; } = new List<string>(); // new List<string>(2) -  רק שני סנסורים לחשיפה
         public List<ISensors> AttachedSensors { get; set; }
 
       
---
+++ InvestigationProject_15_06_25/IranianAgent/Agent/SquadLeader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationProject_15_06_25.IranianAgent.Agent
+{
+    internal class SquadLeader : IIranianAgent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Rank { get; } = "Squad Leader";
+        public List<string> SensitiveToSensors { get; set; } = new List<string>(); // new List<string>(4) -   רק 4 סנסורים לחשיפה
+        public List<ISensors> AttachedSensors { get; set; }
+
+    }
+}
